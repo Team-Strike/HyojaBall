d7ccd1ca6d336b246104f2803b0c61cd404bebf4
JDhyeok <jdhyeok97@gmail.com>
2021-02-16 13:29:42 +0900

    feat: add springboot directory

---
+++ server/src/test/java/hyoja/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package hyoja.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
