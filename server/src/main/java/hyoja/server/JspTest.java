8be66681ef6cba26ad255bd431e490d80387c362
JDhyeok <jdhyeok97@gmail.com>
2021-02-17 15:17:16 +0900

    fix: fix database connection

--- server/src/main/java/hyoja/server/JspTest.java
+++ server/src/main/java/hyoja/server/JspTest.java
@@ -10,15 +10,13 @@ import hyoja.server.board.mapper.HiterMapper;
 @Controller
 public class JspTest {
 
-	@Resource(name="hyoja.server.board.mapper.HiterMapper")
+  @Resource(name="hyoja.server.board.mapper.HiterMapper")
   HiterMapper mHiterMapper;
 
 
-	@RequestMapping("/test")
-	private String jspTest() throws Exception{
-
-		//System.out.println(mHiterMapper.hiterCount()); //DB연동 테스트
-
-		return "test"; //생성한 jsp명(test.jsp)
-	}
+  @RequestMapping("/test")
+  private String jspTest() throws Exception{
+    System.out.println(mHiterMapper.hiterCount()); //DB연동 테스트
+    return "test"; //생성한 jsp명(test.jsp)
+  }
 }
