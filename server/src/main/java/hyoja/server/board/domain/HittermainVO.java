29abb1fdf76db583a250d2ac5837f7518d38a156
genius-jo <joks7285@daum.net>
2021-02-22 03:26:27 +0900

    feature : add Hitter and Pitcher main VO

---
+++ server/src/main/java/hyoja/server/board/domain/HittermainVO.java
@@ -0,0 +1,133 @@
+package hyoja.server.board.domain;
+
+public class HittermainVO {
+
+	int id;
+	String name;
+	String team;
+	int game;
+	int appearance;
+	int at_bat;
+	int hit;
+	int hr;
+	int scored;
+	int rbi;
+	int bb;
+	int strikesouts;
+	int stolen_base;
+	int batting_avg;
+	int obp;
+	int slg;
+	int ops;
+	int war;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	public int getGame() {
+		return game;
+	}
+	public void setGame(int game) {
+		this.game = game;
+	}
+	public int getAppearance() {
+		return appearance;
+	}
+	public void setAppearance(int appearance) {
+		this.appearance = appearance;
+	}
+	public int getAt_bat() {
+		return at_bat;
+	}
+	public void setAt_bat(int at_bat) {
+		this.at_bat = at_bat;
+	}
+	public int getHit() {
+		return hit;
+	}
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+	public int getHr() {
+		return hr;
+	}
+	public void setHr(int hr) {
+		this.hr = hr;
+	}
+	public int getScored() {
+		return scored;
+	}
+	public void setScored(int scored) {
+		this.scored = scored;
+	}
+	public int getRbi() {
+		return rbi;
+	}
+	public void setRbi(int rbi) {
+		this.rbi = rbi;
+	}
+	public int getBb() {
+		return bb;
+	}
+	public void setBb(int bb) {
+		this.bb = bb;
+	}
+	public int getStrikesouts() {
+		return strikesouts;
+	}
+	public void setStrikesouts(int strikesouts) {
+		this.strikesouts = strikesouts;
+	}
+	public int getStolen_base() {
+		return stolen_base;
+	}
+	public void setStolen_base(int stolen_base) {
+		this.stolen_base = stolen_base;
+	}
+	public int getBatting_avg() {
+		return batting_avg;
+	}
+	public void setBatting_avg(int batting_avg) {
+		this.batting_avg = batting_avg;
+	}
+	public int getObp() {
+		return obp;
+	}
+	public void setObp(int obp) {
+		this.obp = obp;
+	}
+	public int getSlg() {
+		return slg;
+	}
+	public void setSlg(int slg) {
+		this.slg = slg;
+	}
+	public int getOps() {
+		return ops;
+	}
+	public void setOps(int ops) {
+		this.ops = ops;
+	}
+	public int getWar() {
+		return war;
+	}
+	public void setWar(int war) {
+		this.war = war;
+	}
+		
+}
---
+++ server/src/main/java/hyoja/server/board/domain/PitchermainVO.java
@@ -0,0 +1,147 @@
+package hyoja.server.board.domain;
+
+public class Pitchermain {
+
+	int id;
+	String name;
+	String team;
+	int win;
+	int lose;
+	int save;
+	int hold;
+	int blown;
+	int game;
+	int starting_pit;
+	int inning;
+	int strikesouts;
+	int bb;
+	int hr;
+	int lob;
+	int era;
+	int ra9_war;
+	int fip;
+	int kfip;
+	int war;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	public int getWin() {
+		return win;
+	}
+	public void setWin(int win) {
+		this.win = win;
+	}
+	public int getLose() {
+		return lose;
+	}
+	public void setLose(int lose) {
+		this.lose = lose;
+	}
+	public int getSave() {
+		return save;
+	}
+	public void setSave(int save) {
+		this.save = save;
+	}
+	public int getHold() {
+		return hold;
+	}
+	public void setHold(int hold) {
+		this.hold = hold;
+	}
+	public int getBlown() {
+		return blown;
+	}
+	public void setBlown(int blown) {
+		this.blown = blown;
+	}
+	public int getGame() {
+		return game;
+	}
+	public void setGame(int game) {
+		this.game = game;
+	}
+	public int getStarting_pit() {
+		return starting_pit;
+	}
+	public void setStarting_pit(int starting_pit) {
+		this.starting_pit = starting_pit;
+	}
+	public int getInning() {
+		return inning;
+	}
+	public void setInning(int inning) {
+		this.inning = inning;
+	}
+	public int getStrikesouts() {
+		return strikesouts;
+	}
+	public void setStrikesouts(int strikesouts) {
+		this.strikesouts = strikesouts;
+	}
+	public int getBb() {
+		return bb;
+	}
+	public void setBb(int bb) {
+		this.bb = bb;
+	}
+	public int getHr() {
+		return hr;
+	}
+	public void setHr(int hr) {
+		this.hr = hr;
+	}
+	public int getLob() {
+		return lob;
+	}
+	public void setLob(int lob) {
+		this.lob = lob;
+	}
+	public int getEra() {
+		return era;
+	}
+	public void setEra(int era) {
+		this.era = era;
+	}
+	public int getRa9_war() {
+		return ra9_war;
+	}
+	public void setRa9_war(int ra9_war) {
+		this.ra9_war = ra9_war;
+	}
+	public int getFip() {
+		return fip;
+	}
+	public void setFip(int fip) {
+		this.fip = fip;
+	}
+	public int getKfip() {
+		return kfip;
+	}
+	public void setKfip(int kfip) {
+		this.kfip = kfip;
+	}
+	public int getWar() {
+		return war;
+	}
+	public void setWar(int war) {
+		this.war = war;
+	}
+		
+}
