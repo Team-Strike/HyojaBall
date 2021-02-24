f6dcccd6dd077948b7d662ea18418cf79c343eb6
ByeongdoChoi <cookie023@naver.com>
2021-02-24 13:34:00 +0900

    feat: add front sturcture

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Mainpage, Teampage, Playerpage } from "./page/index";
+import { Header, Footer } from "./page/components/index";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Route path="/" exact component={Mainpage} />
+        <Route path="/teampage" component={Teampage} />
+        <Route path="/playerpage" component={Playerpage} />
+        <Footer />
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
