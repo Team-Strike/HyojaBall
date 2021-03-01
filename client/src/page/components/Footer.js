b47b17951e767790756ca05bf6b821adf24b22f9
ByeongdoChoi <cookie023@naver.com>
2021-03-01 11:09:28 +0900

    feat: add front home design

--- client/src/page/components/Footer.js
+++ client/src/page/components/Footer.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div>abc</div>;
+  return (
+    <div className="footer">
+      <p>장동혁 조광식 최병도</p>
+    </div>
+  );
 };
 
 export default Footer;
--- client/src/page/components/Header.js
+++ client/src/page/components/Header.js
@@ -2,7 +2,25 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <div>abc</div>;
+  return (
+    <div className="header">
+      <h1>Hyoja Ball</h1>
+      <div className="nav">
+        <div className="team_name">
+          <a>Home &nbsp;</a>
+          <a> 두산 &nbsp;</a>
+          <a> 삼성 &nbsp;</a>
+          <a> 롯데 &nbsp;</a>
+          <a> 키움 &nbsp;</a>
+          <a> 기아 &nbsp;</a>
+          <a> KT &nbsp;</a>
+          <a> LG &nbsp;</a>
+          <a> NC &nbsp;</a>
+          <a> SK &nbsp;</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
